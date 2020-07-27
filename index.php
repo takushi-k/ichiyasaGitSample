21c0df0a18cc4f758067805b7146b4772fdf826a
taku <takutakushii@icloud.com>
2020-07-27 17:12:42 +0900

    p43-2

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 // Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 
+error_log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
 $inputString = file_get_contents('php://input');
 error_log($inputString);
+
+error_log("bbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
 
 ?>
